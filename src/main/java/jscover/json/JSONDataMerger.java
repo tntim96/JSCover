221f0d111480ed0a6c64b74ed3c0bee258e2551c
tntim96 <tntim96@gmail.com>
2012-09-29 12:33:11 +1000

    Add JSON merge class

--- src/main/java/jscover/json/JSONDataMerger.java
+++ src/main/java/jscover/json/JSONDataMerger.java
@@ -338,14 +338,11 @@ proprietary programs.  If your program is a subroutine library, you may
 consider it more useful to permit linking proprietary applications with the
 library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.
-*/
-
+ */
 package jscover.json;
 
-import org.junit.Test;
-
 public class JSONDataMerger {
-    @Test
-    public void should() {
+    public String mergeJSONCoverageData(String data1, String data2) {
+        return null;
     }
 }
