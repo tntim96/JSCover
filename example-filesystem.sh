cff588232d1e0570c7aac2610e846b26cc30f578
tntim96 <tntim96@gmail.com>
2012-10-03 21:05:52 +1000

    Update documentation

--- example-filesystem.sh
+++ example-filesystem.sh
@@ -1,1 +1,1 @@
-java -jar target/dist/JSCover.jar -fs --exclude=qunit/.jshintrc --exclude=test/.jshintrc --no-instrument=test doc/example-qunit/src doc/example-qunit/out
+java -jar target/dist/JSCover.jar -fs --no-instrument=test doc/example-qunit/src doc/example-qunit/out
