8ed88d6dbae5929e3cd9f88ddb1e08859e43b9e6
tntim96 <tntim96@gmail.com>
2012-10-08 17:32:56 +1100

    Remove test code

--- src/test-acceptance/java/jscover/server/HtmlUnitServerTest.java
+++ src/test-acceptance/java/jscover/server/HtmlUnitServerTest.java
@@ -380,11 +380,6 @@ public class HtmlUnitServerTest {
                 }
             });
             server.start();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
         }
     }
 
