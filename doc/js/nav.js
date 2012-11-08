1fbb7444b36b6e7011328fd972d1bf02169ec8cf
tntim96 <tntim96@gmail.com>
2012-11-08 12:34:25 +1100

    Force reload for v0.0.11

--- doc/js/nav.js
+++ doc/js/nav.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         $('#container').html('<div style="position:relative"><h2>Loading...</h2></div>');
         var contentId = $(this).attr("id").replace('Link', '');
         $.ajax({
-            url: contentId + '.html?v=0010',
+            url: contentId + '.html?v=0011',
             dataType:'html',
             success: function(data) {
                 $('#container').html(data+'<p>&nbsp;</p>');
