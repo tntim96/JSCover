7cf57469fbcb838346ccea12c214b0212500061a
tntim96 <tntim96@gmail.com>
2012-12-13 06:34:11 +1100

    Remove cobertura.ser from zip Add useful commands to server sample

--- example-server.bat
+++ example-server.bat
@@ -1,3 +1,5 @@
+rem java -jar target\dist\JSCover-all.jar -ws --branch --document-root=doc/example-qunit/src --report-dir=target/qunit --no-instrument=test
+rem java -cp target/dist/JSCover-all.jar jscover.report.Main --merge target/qunit /doc/example-report-yui3/ target/merged
 rem java -jar target\dist\JSCover-all.jar -ws --branch --document-root=../yui3 --report-dir=target --no-instrument=src/test
 rem java -jar target\dist\JSCover.jar -ws --branch --document-root=doc/example --report-dir=target
 java -jar target\dist\JSCover-all.jar -ws --branch --document-root=doc/example --report-dir=target
