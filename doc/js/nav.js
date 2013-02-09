2b0b1341993f47a3e4d73610cc4f5131b4cbac1e
tntim96 <tntim96@gmail.com>
2013-02-09 20:12:13 +1100

    Update web-site

--- doc/js/nav.js
+++ doc/js/nav.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         $('#container').html('<div style="position:relative"><h2>Loading...</h2></div>');
         var contentId = $(this).attr("id").replace('Link', '');
         $.ajax({
-            url: contentId + '.html?v=023c',
+            url: contentId + '.html?v=023d',
             dataType:'html',
             success: function(data) {
                 $('#container').html(data+'<p>&nbsp;</p>');
