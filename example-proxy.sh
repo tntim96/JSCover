d10832d8d03fb100133c907ece257bdd3591d3cf
tntim96 <tntim96@gmail.com>
2013-02-14 08:07:45 +1100

    Add proxy example and documentation

---
+++ example-proxy.sh
@@ -0,0 +1,1 @@
+$ java -jar target/dist/JSCover-all.jar -ws --branch --proxy --port=3128 --report-dir=target
---
+++ example-ws-for-proxy.bat
@@ -0,0 +1,1 @@
+java -cp target\dist\JSCover-all.jar jscover.server.SimpleWebServer 8080
