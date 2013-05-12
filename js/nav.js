99d09dcab3caf11e7cc7fb66fe63de5058834298
tntim96 <tntim96@gmail.com>
2013-05-12 19:34:13 +1000

    Add link

--- js/nav.js
+++ js/nav.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
     $('#container').html('<div style="position:relative"><h2>Loading...</h2></div>');
     var contentId = $(this).attr("id").replace('Link', '');
     $.ajax({
-      url: contentId + '.html?v=027',
+      url: contentId + '.html?v=027b',
       dataType:'html',
       success: function(data) {
       $('#container').html(data+'<p>&nbsp;</p>');
