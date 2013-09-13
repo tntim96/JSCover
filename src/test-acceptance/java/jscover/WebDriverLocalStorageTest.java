7a3b37df14074f97493b0696dac2b0bbb2f6945e
tntim96 <tntim96@gmail.com>
2013-09-13 17:30:17 +1000

    Fix test name

--- src/test-acceptance/java/jscover/WebDriverLocalStorageTest.java
+++ src/test-acceptance/java/jscover/WebDriverLocalStorageTest.java
@@ -357,7 +357,7 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
 
-public class WebDriverJasmineLocalStorageTest {
+public class WebDriverLocalStorageTest {
     private static Thread server;
 
     protected WebDriver webClient = getWebClient();
