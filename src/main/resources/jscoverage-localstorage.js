63653cd217b732f0e26f7a08fa3afaa562e24646
tntim96 <tntim96@gmail.com>
2013-09-14 22:31:12 +1000

    JavaScript tidying

--- src/main/resources/jscoverage-localstorage.js
+++ src/main/resources/jscoverage-localstorage.js
@@ -1,10 +1,10 @@
-if (typeof(_$jscoverage) == "undefined" && (typeof(Storage) !== "undefined") && localStorage["jscover"])
+if (typeof(_$jscoverage) === "undefined" && (typeof(Storage) !== "undefined") && localStorage["jscover"])
     _$jscoverage = jscoverage_parseCoverageJSON(localStorage["jscover"]);
-if (typeof(jscoverbeforeunload) == "undefined") {
+if (typeof(jscoverbeforeunload) === "undefined") {
     var jscoverbeforeunload = (window.onbeforeunload) ? window.onbeforeunload : function () {};
     window.onbeforeunload = function () {
         jscoverbeforeunload();
         if ((typeof(_$jscoverage) !== "undefined") && (typeof(Storage) !== "undefined"))
-            localStorage["jscover"] = jscoverage_serializeCoverageToJSON()
-    }
+            localStorage["jscover"] = jscoverage_serializeCoverageToJSON();
+    };
 }
