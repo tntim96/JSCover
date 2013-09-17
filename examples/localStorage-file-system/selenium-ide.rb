2171d7724eb9e465fa17fc1b63dfd2fa817f017e
tntim96 <tntim96@gmail.com>
2013-09-17 17:53:51 +1000

    Tidy RSpec code

--- examples/localStorage-file-system/selenium-ide.rb
+++ examples/localStorage-file-system/selenium-ide.rb
@@ -11,12 +11,12 @@ describe "SeleniumIde" do
     @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
   end
-  
+
   after(:each) do
     @driver.quit
     @verification_errors.should == []
   end
-  
+
   it "test_selenium_ide" do
     @driver.get(@base_url + "/index.html")
     @driver.find_element(:id, "radio1").click
@@ -24,19 +24,19 @@ describe "SeleniumIde" do
     @driver.find_element(:id, "radio2").click
     @driver.get(@base_url + "/jscoverage.html")
     coverageJSON = @driver.execute_script("return jscoverage_serializeCoverageToJSON();")
-	File.open '../../target/example-fs-localStorage/jscoverage.json', "w" do |f|
-		f.write coverageJSON
-		f.close
-	end
-	jsCoverJSFile = '../../target/example-fs-localStorage/jscoverage.js'
-	jsCoverJS = File.read(jsCoverJSFile)
-	jsReportCode = "\njscoverage_isReport = true;"
-	if jsCoverJS.index(jsReportCode) == nil
-        File.open jsCoverJSFile, "a" do |f|
-		    f.write jsReportCode
-	    end
-	end
+    coverageJSON = @driver.execute_script("return jscoverage_serializeCoverageToJSON();")
+    File.open '../../target/example-fs-localStorage/jscoverage.json', "w" do |f|
+      f.write coverageJSON
+    end
+    jsCoverJSFile = '../../target/example-fs-localStorage/jscoverage.js'
+    jsCoverJS = File.read(jsCoverJSFile)
+    jsReportCode = "\njscoverage_isReport = true;"
+    if jsCoverJS.index(jsReportCode) == nil
+      File.open jsCoverJSFile, "a" do |f|
+        f.write jsReportCode
+      end
+    end
     @driver.find_element(:id, "summaryTab").click
   end
-  
+
 end
