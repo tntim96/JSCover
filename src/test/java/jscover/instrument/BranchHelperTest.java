0dbfc14dffb5197439572a805a1ed699ca52f62b
tntim96 <tntim96@gmail.com>
2015-07-05 10:47:17 +1000

    Formatting

--- src/test/java/jscover/instrument/BranchHelperTest.java
+++ src/test/java/jscover/instrument/BranchHelperTest.java
@@ -1,8 +1,5 @@
 package jscover.instrument;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Test;
 import org.mozilla.javascript.CompilerEnvirons;
 import org.mozilla.javascript.Context;
@@ -11,65 +8,61 @@ import org.mozilla.javascript.Token;
 import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.AstRoot;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 public class BranchHelperTest {
 
-  private static CompilerEnvirons compilerEnv = new CompilerEnvirons();
-  private BranchHelper helper = new BranchHelper();
-  private Parser parser = new Parser();
+    private static CompilerEnvirons compilerEnv = new CompilerEnvirons();
+    private BranchHelper helper = new BranchHelper();
+    private Parser parser = new Parser();
 
+    static {
+        // compilerEnv.setAllowMemberExprAsFunctionName(true);
+        compilerEnv.setLanguageVersion(Context.VERSION_1_8);
+        compilerEnv.setStrictMode(false);
+    }
 
-  static {
-    // compilerEnv.setAllowMemberExprAsFunctionName(true);
-    compilerEnv.setLanguageVersion(Context.VERSION_1_8);
-    compilerEnv.setStrictMode(false);
-  }
+    @Test
+    public void shouldDetectBoolean() {
+        String script = "x = y || 7;";
+        AstRoot astRoot = parser.parse(script, null, 1);
+        AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
+        AstNode assignNode = NodeTestHelper.findNode(astRoot, Token.ASSIGN);
+        assertThat(helper.isBoolean(orNode), is(true));
+        assertThat(helper.isBoolean(assignNode), is(false));
+    }
 
+    @Test
+    public void shouldDetectCoalesce() {
+        String script = "x = y || {};";
+        AstRoot astRoot = parser.parse(script, null, 1);
+        AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
+        assertThat(helper.isCoalesce(orNode), is(true));
+    }
 
-  @Test
-  public void shouldDetectBoolean() {
-    String script = "x = y || 7;";
-    AstRoot astRoot = parser.parse(script, null, 1);
-    AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
-    AstNode assignNode = NodeTestHelper.findNode(astRoot, Token.ASSIGN);
-    assertThat(helper.isBoolean(orNode), is(true));
-    assertThat(helper.isBoolean(assignNode), is(false));
-  }
+    @Test
+    public void shouldDetectCoalesceWithVariableDeclaration() {
+        String script = "var x = y || {};";
+        AstRoot astRoot = parser.parse(script, null, 1);
+        AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
+        assertThat(helper.isCoalesce(orNode), is(true));
+    }
 
+    @Test
+    public void shouldNotDetectCoalesce() {
+        String script = "if (a || b) ;";
+        AstRoot astRoot = parser.parse(script, null, 1);
+        AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
+        assertThat(helper.isCoalesce(orNode), is(false));
+    }
 
-  @Test
-  public void shouldDetectCoalesce() {
-    String script = "x = y || {};";
-    AstRoot astRoot = parser.parse(script, null, 1);
-    AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
-    assertThat(helper.isCoalesce(orNode), is(true));
-  }
-
-
-  @Test
-  public void shouldDetectCoalesceWithVariableDeclaration() {
-    String script = "var x = y || {};";
-    AstRoot astRoot = parser.parse(script, null, 1);
-    AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
-    assertThat(helper.isCoalesce(orNode), is(true));
-  }
-
-
-  @Test
-  public void shouldNotDetectCoalesce() {
-    String script = "if (a || b) ;";
-    AstRoot astRoot = parser.parse(script, null, 1);
-    AstNode orNode = NodeTestHelper.findNode(astRoot, Token.OR);
-    assertThat(helper.isCoalesce(orNode), is(false));
-  }
-
-
-  @Test
-  public void shouldNotDetectAndAsCoalesce() {
-    String script = "x = y > 7;";
-    AstRoot astRoot = parser.parse(script, null, 1);
-    AstNode node = NodeTestHelper.findNode(astRoot, Token.GT);
-    assertThat(helper.isCoalesce(node), is(false));
-  }
-
+    @Test
+    public void shouldNotDetectAndAsCoalesce() {
+        String script = "x = y > 7;";
+        AstRoot astRoot = parser.parse(script, null, 1);
+        AstNode node = NodeTestHelper.findNode(astRoot, Token.GT);
+        assertThat(helper.isCoalesce(node), is(false));
+    }
 }
--- src/test/java/jscover/instrument/NodeTestHelper.java
+++ src/test/java/jscover/instrument/NodeTestHelper.java
@@ -7,34 +7,34 @@ import org.mozilla.javascript.ast.NodeVisitor;
 
 public class NodeTestHelper {
 
-  static class SearchNodeVisitor implements NodeVisitor {
+    static class SearchNodeVisitor implements NodeVisitor {
 
-    private int type;
-    private AstNode node;
+        private int type;
+        private AstNode node;
 
-    public SearchNodeVisitor(int type) {
-      this.type = type;
+        public SearchNodeVisitor(int type) {
+            this.type = type;
+        }
+
+        public boolean visit(AstNode astNode) {
+            if (astNode.getType() == type) {
+                node = astNode;
+                return false;
+            }
+            return true;
+        }
+
+
+        public AstNode getNode() {
+            return node;
+        }
+
     }
 
-    public boolean visit(AstNode astNode) {
-      if (astNode.getType() == type) {
-        node = astNode;
-        return false;
-      }
-      return true;
+    public static AstNode findNode(AstRoot node, int token) {
+        SearchNodeVisitor visitor = new SearchNodeVisitor(token);
+        node.visitAll(visitor);
+        return visitor.getNode();
     }
-
-
-    public AstNode getNode() {
-      return node;
-    }
-
-  }
-
-  public static AstNode findNode(AstRoot node, int token) {
-    SearchNodeVisitor visitor = new SearchNodeVisitor(token);
-    node.visitAll(visitor);
-    return visitor.getNode();
-  }
 
 }
