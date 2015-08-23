aea351c3fb3b9424f610b0af61bb2838a5f0ad80
tntim96 <tntim96@gmail.com>
2015-08-23 20:33:28 +1000

    Update YUI3 example with JSCover 1.0.21-SNAPSHOT

--- examples/server/jscover-server.sh
+++ examples/server/jscover-server.sh
@@ -1,3 +1,3 @@
 cd ../..
-#java -jar target/dist/JSCover-all.jar -ws --document-root=../yui3 --report-dir=target/example-report-yui3 --no-instrument=src/test
+#java -jar target/dist/JSCover-all.jar -ws --document-root=../yui3 --report-dir=doc/example-report-yui3 --no-instrument=src/test
 java -jar target/dist/JSCover-all.jar -ws --document-root=doc/example --report-dir=target/example-server
