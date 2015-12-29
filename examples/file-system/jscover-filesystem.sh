ecc3183c44e1c763464a2a2425f257ef0a282258
tntim96 <tntim96@gmail.com>
2015-12-29 20:48:53 +1100

    Fix QUnit example

--- examples/file-system/jscover-filesystem.sh
+++ examples/file-system/jscover-filesystem.sh
@@ -1,3 +1,3 @@
 cd ../..
-#java -jar target/dist/JSCover-all.jar -fs --no-instrument=test doc/example-underscore/src doc/example-underscore/out
+#java -jar target/dist/JSCover-all.jar -fs --no-instrument=test doc/example-qunit/src doc/example-qunit/out
 java -jar target/dist/JSCover-all.jar -fs doc/example target/example-fs
