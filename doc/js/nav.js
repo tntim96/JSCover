b1e1b0d35b802a9b41fcc9f4a6438983e0979e36
tntim96 <tntim96@gmail.com>
2017-08-05 12:08:24 +1000

    Fix jscoverproxy-maven-plugin link text

--- doc/js/nav.js
+++ doc/js/nav.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
         $('#container').html('<div style="position:relative"><h2>Loading...</h2></div>');
         var contentId = $(this).attr("id").replace('Link', '');
         $.ajax({
-            url: contentId + '.html?v=202',
+            url: contentId + '.html?v=202b',
             dataType: 'html',
             success: function (data) {
                 $('#container').html(data + '<p>&nbsp;</p>');
